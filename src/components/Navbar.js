9669b5c9f8a81d2549eab2946b7fb8a4e5033519
Adityajamdade1 <adityajamdade2124@gmail.com>
2025-08-16 16:47:15 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -89,7 +89,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/Adityajamdade1"
+                href="https://github.com/Adityajamdade1/Portfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
